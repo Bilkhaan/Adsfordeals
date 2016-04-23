105d8e7edde62190760445cf914d0c842f87e03a
bilal khan <bilal.khan@square63.com>
2016-04-23 19:15:17 +0500

    contentItem: Style and added drop downs to contentitem active admin and seprate the css for it

--- app/admin/content_item.rb
+++ app/admin/content_item.rb
@@ -1,3 +1,17 @@
 ActiveAdmin.register ContentItem do
   permit_params :title, :description, :priority, :ad_type, :coins, :content
+
+  form do |f|
+    f.inputs 'Details' do
+      f.input :title
+      f.input :description
+      f.input :ad_type, as: :select,
+          collection: ContentItem::CONTENT_TYPES.collect {|v| [v, v]}
+      f.input :priority, as: :select,
+          collection: ContentItem::CONTENT_PRIORITIES.collect {|v| [v, v]}
+      f.input :content
+      f.input :coins
+    end
+    f.actions
+  end
 end
--- app/models/content_item.rb
+++ app/models/content_item.rb
@@ -1,2 +1,4 @@
 class ContentItem < ActiveRecord::Base
+  CONTENT_TYPES = %w(web image video)
+  CONTENT_PRIORITIES = %w(featured regular default)
 end
