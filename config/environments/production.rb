66588f9dd0cfeb3f00236693da0fb6e4eb93ffe7
bilal khan <bilalkhaan@hotmail.com>
2016-06-05 23:06:34 +0500

    Added host for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
+  config.action_mailer.default_url_options = { host: 'http://104.236.233.66' }
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
