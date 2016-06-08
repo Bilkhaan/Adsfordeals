2583fd382ebbc8c969168dd9baa27d71f9293369
bilal khan <bilalkhaan@hotmail.com>
2016-06-08 23:39:55 +0500

    Added send grid mail service

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,18 +73,18 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.action_mailer.default_url_options = { host: 'http://contentbunch.com' }
+  config.action_mailer.default_url_options = { host: 'http://adsfordealz.com' }
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => 'bilal.billa.khan1@gmail.com',
-    :password             => 'mobilakh1',
-    :authentication       => "login",
-    :enable_starttls_auto => true,
-    :openssl_verify_mode  => 'none'
+    :user_name => 'bilkhaan',
+    :password => 'mobilakh1',
+    :domain => 'adsfordealz.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
   }
 end
